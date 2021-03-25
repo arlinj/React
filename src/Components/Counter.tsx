1ff95a00fed92a955c3b879c37a31a6bdab962a8
marileidy2829 <marileidycastillo029@gmail.com>
2021-03-25 15:12:49 -0400

    comtenerdor y usuario

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,31 +1,37 @@
-import { useState } from "react"
+import { useState } from 'react';
+
 
 export const Counter = () => {
-          const [Counter, setCounter] = useState(0);
-          const incrementar = ( numero: number = 1 ):void => {
-              setCounter( Counter + numero);
-             
-          }
+
+    const [counter, setCounter] = useState(0);
+
+    const incrementar = ( numero: number = 1 ):void => {
+        setCounter( counter + numero);
+    }
+
 
     return (
-        <div className="nt-5">
-            <h3>Counter:useState</h3>
-            <span>valor: {Counter} </span>
+        <>
+            <h3>Counter:</h3>
+            <span>Valor: { counter } </span>
             <br/>
             <button
-
-            onClick={ () => incrementar() }
-             className="btn btn-outline-primary mt-2">
+                onClick={ () => incrementar(1) } 
+                className="btn btn-outline-primary mt-2">
                 +1
-                </button>
+            </button>
 
-                <button
-
-            onClick={ () => incrementar(2) }
-             className="btn btn-outline-primary mt-2">
+            <button
+                onClick={ () => incrementar(2) } 
+                className="btn btn-outline-primary mt-2">
                 +2
-                </button>
+            </button>
 
-            </div>
+            <button
+                onClick={ () => setCounter(0) } 
+                className="btn btn-outline-danger mt-2">
+                Reset
+            </button>
+        </>
     )
 }
