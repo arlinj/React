2a47b64ff2dfe9346845f6ac5eabd6e46d6b6524
marileidy2829 <marileidycastillo029@gmail.com>
2021-03-25 15:12:24 -0400

    agrehacion del  apartado del usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Counter } from "./Components/Counter";
+import { Usuario } from './Components/Usuario';
 
 function App() {
   return (
     <>
     <h1> react + typeScript </h1>
     <hr/>
-
-    <Counter />
-
+    <h2>useState</h2>
+<Counter />
+<hr/>
+<Usuario/>
       </>
   );
 }
